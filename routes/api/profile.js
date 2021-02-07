9f583b13ab46da5344b0da5fd47f6027f30cf727
Prasad <psdudeman39@gmail.com>
2021-02-07 13:56:48 +0530

    Video 19 - Delete Profile & user, completed. Problem of fetching profiles still exists.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth')
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -105,5 +105,56 @@ router.post('/',
             res.status(500).send('Server Error');
         }
     });
+
+//  @route  GET api/profile
+//  @desc   Get all profiles
+//  @access Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//  @route  GET api/profile/user/:user_id
+//  @desc   Get all profile by ID
+//  @access Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+
+        if (!profile) return res.status(400).json({ msg: "Profile not found" });
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId') {
+            return res.status(400).json({msg: 'Profile not found'});
+        }
+        return res.status(500).send('Server Error');
+    }
+});
+
+//  @route  DELETE api/profile
+//  @desc   Delete profile, user and posts
+//  @access Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        //  @todo - remove users posts
+
+        //  Remove Profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        //  Remove user
+        await Profile.findOneAndRemove({ _id: req.user.id });
+        res.json({ msg: 'User deleted' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 
 module.exports = router;

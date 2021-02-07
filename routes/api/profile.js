bcda05e4fc64060192b5a8a1aaa3284c0d2d9fc1
Prasad <psdudeman39@gmail.com>
2021-02-07 14:52:25 +0530

    Video 20 - Add Profile Experience, complete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const { profile } = require('console');
 
 // @route GET api/profile
 // @desc TEST route
@@ -131,8 +132,8 @@ router.get('/user/:user_id', async (req, res) => {
         res.json(profile);
     } catch (err) {
         console.error(err.message);
-        if(err.kind == 'ObjectId') {
-            return res.status(400).json({msg: 'Profile not found'});
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found' });
         }
         return res.status(500).send('Server Error');
     }
@@ -156,5 +157,56 @@ router.delete('/', auth, async (req, res) => {
     }
 });
 
+//  @route  PUT api/profile/experience
+//  @desc   Add profile experience
+//  @access Private
+router.put('/experience', [auth, [
+    check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+    check('company', 'Company is required')
+        .not()
+        .isEmpty(),
+    check('from', 'From date is required')
+        .not()
+        .isEmpty(),
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        profile.experience.unshift(newExp); //  pushes it onto the front.
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;

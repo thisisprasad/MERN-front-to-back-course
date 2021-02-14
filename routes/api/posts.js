28e9139200eaf8420e7b6ea0ef8a960f9c19ea53
Prasad <psdudeman39@gmail.com>
2021-02-14 13:33:56 +0530

    Get & Delete Post Routes, completed

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -14,7 +14,7 @@ router.post('/', [auth, [
     check('text', 'Text is required').not().isEmpty()
 ]], async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) {
+    if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
@@ -33,6 +33,71 @@ router.post('/', [auth, [
         res.json(post);
     } catch (err) {
         console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route GET api/posts
+// @desc Get all posts
+// @access Private
+router.get('/', auth, async (req, res) => {
+    
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route GET api/posts
+// @desc Get post by ID
+// @access Private
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post) {
+            return res.status(404).json({msg: 'Post not found'});
+        }
+
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind === 'ObjectId') {
+            return res.status(404).json({msg: 'Post not found'});
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route DELETE api/posts/:id
+// @desc Delete a post
+// @access Private
+router.delete('/:id', auth, async (req, res) => {
+    
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //  check post
+        if(!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        //  Check on the user.
+        if(post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+
+        await post.remove();
+
+        res.json({ msg: 'Post removed' });
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind === 'ObjectId') {
+            return res.status(404).json({msg: 'Post not found'});
+        }
         res.status(500).send('Server Error');
     }
 });

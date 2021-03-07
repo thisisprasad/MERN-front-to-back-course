ebde762632822256dadbe1b830bcad4bdb04b327
Prasad <psdudeman39@gmail.com>
2021-03-07 20:06:04 +0530

    Video 43, Logout & Navbar Links, completed

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL
+    LOGIN_FAIL,
+    LOGOUT
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -93,3 +94,8 @@ export const login = (email, password) => async dispatch => {
         });
     }
 }
+
+//  Logout / Clear Profile
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT });
+};
